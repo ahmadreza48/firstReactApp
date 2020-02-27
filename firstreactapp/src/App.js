6d0c81925981b73188ea2ce32d7fe430f9465fd0
ahmadreza <ahmadreza.saeidi48@gmail.com>
2020-02-27 17:16:43 +0330

    first child component add...

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -1,15 +1,19 @@
 import React from "react";
+
+import Person from './components/Person';
 import "./App.css";
 
 class App extends React.Component {
-  constructor() {
-    super();
-  }
+  // constructor() {
+  //   super();
+  // }
 
   render() {
     return (
       <div className="App">
         <h1>Hello World!</h1>
+        <hr/>
+        <Person/>
       </div>
     );
   }
---
+++ firstreactapp/src/components/Person.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class Person extends Component {
+    state = {  }
+    render() { 
+        return (<p>ahmadreza saeidi</p>);
+    }
+}
+ 
+export default Person;
