e90669810d9217676251821e802e817c664eaac5
ahmadreza <ahmadreza.saeidi48@gmail.com>
2020-02-27 17:38:39 +0330

    change class components to function components...

--- firstreactapp/src/App.js
+++ firstreactapp/src/App.js
@@ -1,22 +1,35 @@
+// import React, { Component } from 'react';
 import React from "react";
 
-import Person from './components/Person';
+import Person from "./components/Person";
 import "./App.css";
 
-class App extends React.Component {
-  // constructor() {
-  //   super();
-  // }
+// class App extends React.Component {
+//   // constructor() {
+//   //   super();
+//   // }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hello World!</h1>
-        <hr/>
-        <Person/>
-      </div>
-    );
-  }
-}
+//   render() {
+//     return (
+//       <div className="App">
+//         <h1>Hello World!</h1>
+//         <hr/>
+//         <Person/>
+//       </div>
+//     );
+//   }
+// }
+
+// export default App;
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>Hello World!</h1>
+      <hr />
+      <Person />
+    </div>
+  );
+};
 
 export default App;
--- firstreactapp/src/components/Person.jsx
+++ firstreactapp/src/components/Person.jsx
@@ -1,10 +1,21 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 
-class Person extends Component {
-    state = {  }
-    render() { 
-        return (<p>ahmadreza saeidi</p>);
-    }
-}
+// class Person extends Component {
+//     state = {  }
+//     render() { 
+//         return (<p>ahmadreza saeidi</p>);
+//     }
+// }
  
+// export default Person;
+
+
+import React from 'react';
+
+const Person = () => {
+    return(
+        <p>ahmadreza saeidi</p>
+    )
+}
+
 export default Person;
