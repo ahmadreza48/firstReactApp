7619fccb9612a8cddcd51f1d7d85652c10dbfb32
ahmadreza <ahmadreza.saeidi48@gmail.com>
2020-03-04 00:15:20 +0330

    new project added..

---
+++ personmanager/src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+    state = {  }
+    render() { 
+        return ( <div>
+            <h1>hame chiz khub ast!</h1>
+        </div> );
+    }
+}
+ 
+export default App;
